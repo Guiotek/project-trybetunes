52d4b98cffb406fb17d302b75829482768d97645
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-09-01 18:37:23 -0300

    requisito 4

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -30,7 +31,14 @@ export default class Header extends Component {
       <header data-testid="header-component">
         {
           (loading ? <Loading />
-            : <p data-testid="header-user-name">{user.name}</p>)
+            : (
+              <>
+                <p data-testid="header-user-name">{user.name}</p>
+                <Link to="/search" data-testid="link-to-search">Search</Link>
+                <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+                <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+
+              </>))
         }
 
       </header>
